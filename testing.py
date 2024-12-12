9a7404397993d2948f8528110bf027452603eaa7
Michelle607 <106538180+Michelle607@users.noreply.github.com>
2024-12-12 17:44:37 +0900

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,13 @@
+import torch
+print(torch.__version__)
+print(torch.cuda.is_available())
+print(torch.version.cuda)
+
+"""
+E:\anaconda3\envs\style\python.exe E:\downloads\PycharmProjects_E\stylegan2-ada-pytorch\testing.py 
+1.7.1
+True
+11.0
+
+Process finished with exit code 0
+"""
